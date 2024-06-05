257b64e4bd401dd23e8506b0bb85018c48299fde
VishalKumar1111 <aryan11gs@gmail.com>
2024-06-05 14:55:08 +0530

    Class Based Views

--- operation/admin.py
+++ operation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from operation.models import *
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','name','roll','city']
--- operation/views.py
+++ operation/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from operation.models import Student
+from operation.serializers import StudentSerializer
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views import View
+@method_decorator(csrf_exempt,name='dispatch')
+class StudentAPI(View):
+    def get(self,request,*args,**kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id', None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
 
-# Create your views here.
+    def post(self,request,*args,**kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Inserted'}
+            # converted into JSON
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+    def put(self,request,*args,**kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id)
+        #partial update
+        serializer=StudentSerializer(stu,data=pythondata,partial=True)#stores complex data or model objexts
+        #serializer=StudentSerializer(stu,data=pythondata) #fully Updated
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Updated'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+    
+    def delete(self,request,*args,**kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id) #object get
+        stu.delete()
+        res = {'msg': 'Data Deleted'}
+        #json_data = JSONRenderer().render(res)
+        #return HttpResponse(json_data, content_type='application/json')
+        #or
+        return JsonResponse(res,safe=False)
+
+
